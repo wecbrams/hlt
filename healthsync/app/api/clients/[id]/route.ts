ee28efea7825e4252d64dcce5b22144c37f268f1
wecbrams <97198443+wecbrams@users.noreply.github.com>
2025-04-28 15:21:41 +0300

    Update route.ts

--- healthsync/app/api/clients/[id]/route.ts
+++ healthsync/app/api/clients/[id]/route.ts
@@ -1,8 +1,10 @@
 import { prisma } from '@/lib/prisma';
 import { NextRequest, NextResponse } from 'next/server';
-import type { RouteHandlerContext } from 'next';
 
-export async function GET(req: NextRequest, context: RouteHandlerContext<{ id: string }>) {
+export async function GET(
+  req: NextRequest,
+  context: { params: { id: string } }
+) {
   const { id } = context.params;
 
   if (!id) {
