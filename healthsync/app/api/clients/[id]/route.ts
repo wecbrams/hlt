e0499bf4bf33682aff3093cc236b0e5ac169d428
wecbrams <97198443+wecbrams@users.noreply.github.com>
2025-04-28 15:52:47 +0300

    Up route.ts

--- healthsync/app/api/clients/[id]/route.ts
+++ healthsync/app/api/clients/[id]/route.ts
@@ -1,11 +1,9 @@
 import { prisma } from '@/lib/prisma';
 import { NextRequest, NextResponse } from 'next/server';
 
-export async function GET(
-  req: NextRequest,
-  context: { params: { id: string } }
-) {
-  const { id } = context.params;
+export async function GET(req: NextRequest) {
+  const url = new URL(req.url);
+  const id = url.pathname.split('/').pop();
 
   if (!id) {
     return NextResponse.json({ error: 'Client ID missing' }, { status: 400 });
